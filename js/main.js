14333eb76082f8858011516ebdc9aafbde15b673
Zual Rana <freelancerzual@gmail.com>
2024-11-26 19:18:59 +0600

    mobile menu hide outside click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const toggle = document.getElementById("toggle-menu");
 const menu = document.getElementById("mobile-menu");
-toggle.addEventListener("click", () => {
+toggle.addEventListener("click", (e) => {
   menu.classList.toggle("show");
+  e.stopPropagation();
+});
+document.addEventListener("click", (event) => {
+  if (
+    menu.classList.contains("show") &&
+    !menu.contains(event.target) &&
+    event.target !== toggle
+  ) {
+    menu.classList.remove("show");
+  }
 });
