3a63e08145a055a009faf9e4eaba084ed39300a3
Zual Rana <freelancerzual@gmail.com>
2024-11-26 19:48:13 +0600

    toggle button changed to exit button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ const toggle = document.getElementById("toggle-menu");
 const menu = document.getElementById("mobile-menu");
 toggle.addEventListener("click", (e) => {
   menu.classList.toggle("show");
+  if (menu.classList.contains("show")) {
+    toggle.innerHTML = `<p style="font-size: 1.5rem;">✖</p>`; // Change to "exit" icon
+  } else {
+    toggle.innerHTML = ` <span></span><span></span>
+            <span></span>`; // Change back to "menu" icon
+  }
+
   e.stopPropagation();
 });
 document.addEventListener("click", (event) => {
@@ -11,5 +18,7 @@ document.addEventListener("click", (event) => {
     event.target !== toggle
   ) {
     menu.classList.remove("show");
+    toggle.innerHTML = `<span></span>
+            <span></span><span></span>`;
   }
 });
